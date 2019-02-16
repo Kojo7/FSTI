b050ac89ceb59395b3ce77fa7b24b51259a1c141
Kojo7 <42452199+Kojo7@users.noreply.github.com>
2019-02-16 14:12:22 +0100

    Add files via upload
    Edited

--- IPrint/hackhaton.sol
+++ IPrint/hackhaton.sol
@@ -14,21 +14,26 @@ contract owner is Ownable {
     //address[] public Hashowners;
     
     mapping (address => string) public printOwners; // Mapping address of owner to HashValues;
-    //mapping (string => address) hashowner; // Mapping hash value to owners;
+    mapping (string => address) hashowner; // Mapping hash value to owners;
     //mapping (address => HashOwner) owners; // List of all owners and hash values;
     
-
+    //set hash value and address of owner of HashValue
     function setHashValue(string _Hash, address _Address) external {
-        //owners[msg.sender].Hash = _Hash;
         Hash = _Hash;
         Address = _Address;
         printOwners[_Address] = _Hash;
-        //hashowner[_Hash] = _Address;
+        hashowner[_Hash] = _Address;
         
     }
     
-    //function getHashValue(address _address) view external returns(string) {
-      //  return (owners[_address].Hash);
-    //}
+    //get the hash value by address
+    function getHashValue(address _Address) view external returns(string) {
+       return Hash;
+    }
+    
+    //get the address by hash value
+    function getAddress(string _Hash) view external returns(address) {
+        return Address;
+    }
     
 }
